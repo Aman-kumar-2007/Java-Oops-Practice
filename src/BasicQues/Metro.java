5e33682ec9b317d2eb1d4b4200e7c7d1193f9f23
Aman Kumar <amanthecoder001@gmail.com>
2026-06-09 21:12:01 +0530

    OverRide concept ques

---
+++ src/BasicQues/Metro.java
@@ -0,0 +1,63 @@
+package BasicQues;
+/*
+Transport System
+
+Parent:
+
+Vehicle
+fare()
+
+Child:
+
+Bus
+Train
+Metro
+
+Override fare().
+
+ */
+class Vehicle {
+
+    void fare() {
+        System.out.println("Calculating Fare");
+    }
+}
+
+class Bus extends Vehicle {
+
+    @Override
+    void fare() {
+        System.out.println("Calculating Bus Fare");
+    }
+}
+
+class Train extends Vehicle {
+
+    @Override
+    void fare() {
+        System.out.println("Calculating Train Fare");
+    }
+}
+
+public class Metro extends Vehicle {
+
+    @Override
+    void fare() {
+        System.out.println("Calculating Metro Fare");
+    }
+
+    public static void main(String[] args) {
+
+        Vehicle v;
+
+        v = new Bus();
+        v.fare();
+
+        v = new Train();
+        v.fare();
+
+        v = new Metro();
+        v.fare();
+
+    }
+}
