2b92252ae9a2fc966900c0c7b5b233e81f7f0325
Aman Kumar <amanthecoder001@gmail.com>
2026-06-09 19:03:55 +0530

    done with one more ques

---
+++ src/BasicQues/Laptop.java
@@ -0,0 +1,30 @@
+package BasicQues;
+
+/*
+Create a class Laptop
+
+Data members:
+
+brand
+price
+
+Initialize values using a constructor.
+
+ */
+public class Laptop {
+    String brand;
+    int price;
+    Laptop(String brand,int price){
+        this.brand = brand;
+        this.price = price;
+
+    }
+
+    public static void main(String[] args){
+        Laptop l1 = new Laptop("Lenevo",89000);
+        Laptop l2 = new Laptop("Lenevo",90000);
+
+        System.out.println(l1.brand + " " + l1.price);
+        System.out.println(l2.brand + " " + l2.price);
+    }
+}
