e282cf783fdc548f8b10de3fe4a3cbe4af2f4537
Aman Kumar <amanthecoder001@gmail.com>
2026-06-15 20:31:51 +0530

    added getter and setter for hospital patient details

--- src/QuestionForExamPrep/Hospital.java
+++ src/QuestionForExamPrep/Hospital.java
@@ -1,5 +1,33 @@
 package QuestionForExamPrep;
 
 public class Hospital {
-    Private int patientId;
+    private int patientId;
+    private String patienName;
+    private int age;
+
+    void setPatientId(int id){
+        this.patientId = id;
+    }
+
+    void setPatienName(String name){
+        this.patienName = name;
+    }
+
+    void setAge(int age){
+        this.age = age;
+    }
+
+    int getPatientId(){
+        return this.patientId;
+    }
+
+    int getAge(){
+        return this.age;
+    }
+
+    String getPatienName(){
+        return this.patienName;
+    }
+
+
 }
