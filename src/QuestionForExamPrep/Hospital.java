6c1ade0378b039c6c8c381da5b5adf9b4751a44a
Aman Kumar <amanthecoder001@gmail.com>
2026-06-15 20:44:25 +0530

    completed project of hospital management system

--- src/QuestionForExamPrep/Hospital.java
+++ src/QuestionForExamPrep/Hospital.java
@@ -2,19 +2,22 @@ package QuestionForExamPrep;
 
 public class Hospital {
     private int patientId;
-    private String patienName;
+    private String patientName;
     private int age;
 
     void setPatientId(int id){
         this.patientId = id;
     }
 
-    void setPatienName(String name){
-        this.patienName = name;
+    void setPatientName(String name){
+        if(name != null && !name.isEmpty() )
+            this.patientName = name.trim();
+        else System.out.println("name can't be empty");
     }
 
     void setAge(int age){
-        this.age = age;
+        if(age < 0) System.out.println("age can't be neagtive");
+        else this.age = age;
     }
 
     int getPatientId(){
@@ -25,9 +28,17 @@ public class Hospital {
         return this.age;
     }
 
-    String getPatienName(){
-        return this.patienName;
+    String getPatientName(){
+        return this.patientName;
     }
 
+    public static void main(String[] args){
+        Hospital a = new Hospital();
+        a.setPatientName("Aman");
+        a.setPatientId(1);
+        a.setAge(18);
 
+        System.out.println(a.getPatientName());
+
+    }
 }
