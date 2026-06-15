55ac370a98012a7162e9538914c581796e67d295
Aman Kumar <amanthecoder001@gmail.com>
2026-06-16 02:32:43 +0530

    made a cricket player list with highest and avg score

---
+++ src/QuestionForExamPrep/Cricket.java
@@ -0,0 +1,45 @@
+package QuestionForExamPrep;
+
+class Player{
+    String playerName;
+    int runs;
+
+    Player(String name,int run){
+        this.playerName = name;
+        this.runs = run;
+    }
+}
+public class Cricket {
+    public static void main(String[] args){
+
+        Player[] players = new Player[5];
+
+        players[0] = new Player("Virat", 85);
+        players[1] = new Player("Rohit", 120);
+        players[2] = new Player("Gill", 75);
+        players[3] = new Player("Hardik", 95);
+        players[4] = new Player("Rahul", 60);
+
+        System.out.println("Player Details");
+
+        Player highest =  players[0]; // data Type player hai
+        int totalRuns = 0;
+        for(Player p : players){
+            System.out.println("Player Name : " + p.playerName + " \nruns : " + p.runs);
+            totalRuns += p.runs;
+
+            if(p.runs > highest.runs) {
+                highest = p;
+            }
+
+        }
+
+        double average = (double) totalRuns / players.length;
+
+        System.out.println("Highest Scorer : " + highest.playerName + " -> " + highest.runs);
+
+        System.out.println("Average Runs : " + average);
+
+
+    }
+}
