15fa235ca3726135744b1f06ca3b058ddc2882f9
Aman Kumar <amanthecoder001@gmail.com>
2026-06-23 23:02:38 +0530

    ques on atm history

---
+++ src/QuesOnJavaCollectionFrameworks/ATM.java
@@ -0,0 +1,16 @@
+package QuesOnJavaCollectionFrameworks;
+
+/*
+ATM Transaction History
+
+Requirements:
+
+Store transactions.
+Add transactions.
+Display all transactions.
+Display total transactions.
+
+ */
+public class ATM {
+
+}
