bb57312137ad9f6f5e7726a063c8cde71ed66c82
Aman Kumar <amanthecoder001@gmail.com>
2026-06-23 23:03:30 +0530

    ques on atm history code done

--- src/QuesOnJavaCollectionFrameworks/ATM.java
+++ src/QuesOnJavaCollectionFrameworks/ATM.java
@@ -1,5 +1,7 @@
 package QuesOnJavaCollectionFrameworks;
 
+import java.util.ArrayList;
+
 /*
 ATM Transaction History
 
@@ -12,5 +14,24 @@ Display total transactions.
 
  */
 public class ATM {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> transactions = new ArrayList<>();
+
+        transactions.add("Deposit Rs.5000");
+        transactions.add("Withdraw Rs.1000");
+        transactions.add("Balance Check");
+        transactions.add("Deposit Rs.2000");
+
+        System.out.println("Transaction History:");
+
+        for(String t : transactions){
+            System.out.println(t);
+        }
+
+        System.out.println("Total Transactions = "
+                + transactions.size());
+    }
 
 }
