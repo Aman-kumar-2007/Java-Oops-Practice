71c49caaedb389b00d9d08075ce266b41fc129ca
Aman Kumar <amanthecoder001@gmail.com>
2026-06-24 21:31:10 +0530

    msg

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,6 @@
 void main() {
     // to see how IntelliJ IDEA suggests fixing it.
     IO.println(String.format("Hello and welcome!"));
-
-    for (int i = 1; i <= 5; i++) {
-        IO.println("i = " + i);
-    }
+    IO.println("Lets !! Practice JAVA OOPs");
 }
 
